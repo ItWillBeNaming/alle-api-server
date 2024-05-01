c7dfa7d70275a61b5bac61227d2451a8d8a382b1
Sunro Lee <tjsfhdhkd@naver.com>
2024-05-02 02:13:25 +0900

    Like 엔티티 생성

---
+++ src/main/java/com/alle/api/domain/board/domain/Like.java
@@ -0,0 +1,29 @@
+package com.alle.api.domain.board.domain;
+
+import com.alle.api.domain.member.domain.Member;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "like")
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Like {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id", nullable = false)
+    private Board board;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+}
