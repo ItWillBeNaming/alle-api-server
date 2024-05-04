6831a13cd3babafac25c27aa727060408da7d8f9
Sunro Lee <tjsfhdhkd@naver.com>
2024-05-04 17:10:56 +0900

    Swagger Config

---
+++ src/main/java/com/alle/api/global/config/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package com.alle.api.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        License license = new License();
+        license.setName("Olle");
+
+        Info info = new Info()
+                .title("\"Olle API Document\"")
+                .description("Olleh API 문서 입니다.")
+                .version("v0.0.1")
+                .license(license);;
+
+        String jwt = "JWT";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt); // 헤더에 토큰 포함
+        Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
+                .name(jwt)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT"));
+
+        return new OpenAPI()
+                .info(info)
+                .addSecurityItem(securityRequirement)
+                .components(components)
+                .addSecurityItem(new SecurityRequirement().addList(jwt));
+
+    }
+
+}
+
