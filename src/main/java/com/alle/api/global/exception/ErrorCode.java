d111ffcdc9f84bc1121c5be9b33074fad13b4df9
Sunro Lee <tjsfhdhkd@naver.com>
2024-05-04 17:05:45 +0900

    Error Enum 필드 추가

--- src/main/java/com/alle/api/global/exception/ErrorCode.java
+++ src/main/java/com/alle/api/global/exception/ErrorCode.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+import static org.springframework.http.HttpStatus.*;
 import static org.springframework.http.HttpStatus.CONFLICT;
 
 @Getter
@@ -12,6 +13,10 @@ public enum ErrorCode {
 
     // TODO i18n
     DUPLICATE_RESOURCE(CONFLICT, "error.duplicate.resource"),
+
+    INVALID_PARAMETER(BAD_REQUEST, "잘못된 요청 데이터 입니다."),
+    INVALID_AUTHENTICATION(BAD_REQUEST, "잘못된 인증입니다.");
+
     ;
 
     private final HttpStatus httpStatus;
