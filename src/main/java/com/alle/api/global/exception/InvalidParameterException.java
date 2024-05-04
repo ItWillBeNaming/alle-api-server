22701cafdb3066c486964aebc120683ae93cd92a
Sunro Lee <tjsfhdhkd@naver.com>
2024-05-04 17:06:47 +0900

    유효하지 않은 파라미터 Exception 클래스

---
+++ src/main/java/com/alle/api/global/exception/InvalidParameterException.java
@@ -0,0 +1,23 @@
+package com.alle.api.global.exception;
+
+import lombok.Getter;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
+
+import java.util.List;
+
+@Getter
+public class InvalidParameterException extends DefaultException{
+
+    private final Errors errors;
+
+    public InvalidParameterException(Errors errors) {
+        super(ErrorCode.INVALID_PARAMETER);
+        this.errors = errors;
+    }
+
+    public List<FieldError> getFieldErrors() {
+        return errors.getFieldErrors();
+    }
+    
+}
