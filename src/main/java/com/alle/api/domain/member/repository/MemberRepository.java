2d375cefe6e46ea22a689cbe65d56d305d22fd0c
Sunro Lee <tjsfhdhkd@naver.com>
2024-05-04 17:09:00 +0900

    회원 조회 메서드 추가

--- src/main/java/com/alle/api/domain/member/repository/MemberRepository.java
+++ src/main/java/com/alle/api/domain/member/repository/MemberRepository.java
@@ -4,5 +4,12 @@ package com.alle.api.domain.member.repository;
 import com.alle.api.domain.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByLoginId(String loginId);
+
+    boolean existsByLoginId(String loginId);
+
 }
