cf7587bf1c607cfd99fd2f9872b5d5c40f8fcb59
Sunro Lee <tjsfhdhkd@naver.com>
2024-06-03 20:15:53 +0900

    ExceptionCode code별로 정리

--- src/main/java/com/alle/api/global/exception/ExceptionCode.java
+++ src/main/java/com/alle/api/global/exception/ExceptionCode.java
@@ -12,18 +12,13 @@ public enum ExceptionCode {
 
     NOT_HANDLED_EXCEPTION(INTERNAL_SERVER_ERROR, "Error", 900),
 
-    DUPLICATE_RESOURCE(CONFLICT, "error.duplicate.resource", 1),
-    INVALID_PARAMETER(BAD_REQUEST, "BAD Request", 2),
-    INVALID_AUTHENTICATION(UNAUTHORIZED, "잘못된 인증입니다.", 3),
-    MALFORMED_TOKEN(UNAUTHORIZED, "Malformed Token", 5),
-    EXPIRED_TOKEN(UNAUTHORIZED, "Expired Token", 6),
-    NOT_FOUND_MEMBER(CONFLICT,"Can't find Member",7),
 
     // 400
     PASSWORD_MISMATCH(BAD_REQUEST, "Passwords do not match.", 400),
     INVALID_CURRENT_PASSWORD(BAD_REQUEST, "The current password does not match.", 400),
     INVALID_AUTH_CODE(BAD_REQUEST, "Email authentication failed.", 400),
     NOT_FOUND_REFRESH_TOKEN_IN_COOKIE(BAD_REQUEST, "Refresh token is not found in cookie.", 400),
+    INVALID_PARAMETER(BAD_REQUEST, "BAD Request", 400),
 
     // 401
     UNAUTHORIZED_LOGIN(UNAUTHORIZED, "Login failed: authentication failed.", 401),
@@ -32,9 +27,16 @@ public enum ExceptionCode {
     UNSUPPORTED_TOKEN(UNAUTHORIZED, "Unsupported token format.", 401),
     NOT_FOUND_TOKEN(UNAUTHORIZED, "Token not found.", 401),
     NOT_FOUND_REFRESH_TOKEN(UNAUTHORIZED, "Refresh token for the user not found.", 401),
-    MEMBER_ALREADY_EXISTS(CONFLICT,"Member Already Exists" , 403 ),
-    NICKNAME_ALREADY_EXISTS(CONFLICT, "Nickname already exists" , 403 );
+    INVALID_AUTHENTICATION(UNAUTHORIZED, "잘못된 인증입니다.", 401),
+    MALFORMED_TOKEN(UNAUTHORIZED, "Malformed Token", 401),
+    EXPIRED_TOKEN(UNAUTHORIZED, "Expired Token", 401),
 
+    DUPLICATE_RESOURCE(CONFLICT, "error.duplicate.resource", 403),
+    NOT_FOUND_MEMBER(CONFLICT,"Can't find Member",403),
+    NOT_FOUND_BOARD(CONFLICT,  "Can't find Board" ,403 ),
+    MEMBER_ALREADY_EXISTS(CONFLICT,"Member Already Exists" , 403 ),
+    NICKNAME_ALREADY_EXISTS(CONFLICT, "Nickname already exists" , 403 ),
+    NOT_MATCHED_WRITER(CONFLICT,"Not Matched Writer" ,403 );
 
 
     private final HttpStatus httpStatus;
